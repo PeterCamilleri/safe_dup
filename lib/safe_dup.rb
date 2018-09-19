9a3cf504ee2a39e598b6d8c08df3a754b117e5b7
PeterCamilleri <peter.c.camilleri@gmail.com>
2018-09-18 23:20:03 -0400

    Removed code smells with reek.

--- lib/safe_dup.rb
+++ lib/safe_dup.rb
@@ -2,47 +2,52 @@
 
 require_relative "safe_dup/version"
 
-# Now you too can dup without fear of annoying exceptions!
-
+# Now you too can dup objects without fear of annoying exceptions!
 class Object
-  #By default, reference types use the clone method.
+  #By default, reference types use the dup method.
   def safe_dup
     dup
   end
 end
 
-#For value types, just return self!
+# For value types, just return self!
 module SafeDup
   def safe_dup
     self
   end
 end
 
-#Update the Ruby value types.
+# Numeric can now safe dup.
 class Numeric
   include SafeDup
 end
 
+# NilClass can now safe dup.
 class NilClass
   include SafeDup
 end
 
+# TrueClass can now safe dup.
 class TrueClass
   include SafeDup
 end
 
+# FalseClass can now safe dup.
 class FalseClass
   include SafeDup
 end
 
+# Symbols can now safe dup.
 class Symbol
   include SafeDup
 end
 
+# Regular expressions can now safe dup.
 class Regexp
   include SafeDup
 end
 
+# Threads can now safe dup.
 class Thread
   include SafeDup
 end
